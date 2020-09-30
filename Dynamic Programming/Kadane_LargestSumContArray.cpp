a88e23b48432b229e4f14221b3bb2ac3ee882b36
shreayan98c <shreayan98c@gmail.com>
2020-09-30 14:03:58 +0530

    Updated the question descriptions and added new questions

--- Dynamic Programming/Kadane_LargestSumContArray.cpp
+++ Dynamic Programming/Kadane_LargestSumContArray.cpp
@@ -1,6 +1,21 @@
 // Largest Sum Contiguous Subarray
 
-// Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum.
+// Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum. A contiguous subarray is a subset of the array that includes the elements in a sequenced manner, without skipping any element in between.
+// Example of contiguous subarrays for the array [1,2,3,4,5]
+// [2,3,4]
+// [4,5]
+// [1,2,3,4,5]
+// [1,2,3]
+
+// Sample Input:
+// 6
+// 1 -2 1 1 -2 1
+
+// Output:
+// 2
+
+// Explanation: 
+// The max contiguous sum in the above array will be 2, starting from index 2 and ending at index 3. All other contiguous array sums will be less than 2.
 
 #include <iostream>
 using namespace std;
