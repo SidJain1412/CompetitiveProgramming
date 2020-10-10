de808e5f312281629864187e05d703442ca3ee47
Bahroze Ali <61082015+bahroze-dev@users.noreply.github.com>
2020-10-10 13:24:32 +0500

    Update RecursiveInsersationSort.py

--- Recursion/RecursiveInsersationSort.py
+++ Recursion/RecursiveInsersationSort.py
@@ -26,14 +26,20 @@ printArray(arr, n)
 def insertionSortRecursive(arr,n):
     if n<=1:
         return
+    # Sort first n-1 elements 
     insertionSortRecursive(arr,n-1)
     last = arr[n-1]
     j = n-2
+    
+    # Move elements of arr[0..i-1], that are 
+    # greater than key, to one position ahead 
+    # of their current position  
     while (j>=0 and arr[j]>last):
         arr[j+1] = arr[j]
         j = j-1
     arr[j+1]=last
      
+# a function that prints array of certain size
 def printArray(arr,n):
     for i in range(n):
         print arr[i],
