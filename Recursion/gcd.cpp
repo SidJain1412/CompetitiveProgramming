b5c7e11aa34bde01a910a94ca446edb3db07efd5
Vishal Burman <vishal.a.burman23@gmail.com>
2020-10-11 00:44:39 +0530

    gcd.cpp: Changed to Euclid's algorithm for efficiency (#64)

--- Recursion/gcd.cpp
+++ Recursion/gcd.cpp
@@ -4,20 +4,14 @@
 using namespace std;
 
 int gcd(int a, int b) {
-   if (a == 0 || b == 0){
-   	return 0;
-   }
-   else if (a == b){
+   if (b == 0){
    	return a;
-   }
-   else if (a > b){
-   	return gcd(a-b, b);
-   }
-   else return gcd(a, b-a);
+   } 
+   return gcd(b, a % b);
 }
 
 int main() {
    int a = 92, b = 36;
-   cout<<"GCD of "<< a <<" and "<< b <<" is "<< gcd(a, b);
+   cout<<"GCD of "<< a <<" and "<< b <<" is "<< gcd(a, b) << endl;
    return 0;
 }
