f5ecd3be20ed8ba4443835be0702eabc275fe090
prasath <pk9515@srmist.edu.in>
2020-10-26 11:25:36 +0530

    Added Ques, i/p and o/p

--- Backtracking/NQueens.cpp
+++ Backtracking/NQueens.cpp
@@ -1,3 +1,35 @@
+/*Problem Name: NQueens
+
+Problem Description:
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+There exist two distinct solutions to the 4-queens puzzle as shown above.
+
+INPUT: 4
+
+OUTPUT:
+[ #Solution1
+[".Q..",
+"...Q",
+"Q...",
+"..Q."],
+
+CONSTRAINTS:
+
+Sample input: 4
+
+Sample output:
+["..Q.",
+"Q...", #solution2
+"...Q",
+".Q.."]
+]*/
+
+
+
+
+
 #include<iostream>
 using namespace std;
 #define N 4
